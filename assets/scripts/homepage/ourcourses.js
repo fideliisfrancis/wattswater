cb07d021d35048bc4a3ee253308ea2ab909211f1
fideliisfrancis <fideliisfrancis@yahoo.com>
2023-07-31 12:59:29 +0530

    Watts water Reference Commit

---
+++ assets/scripts/homepage/ourcourses.js
@@ -0,0 +1,67 @@
+ //Slick Slider 
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+
+  });
+
+  
+  /*$('.course-slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });*/
---
+++ gulpfile.js
@@ -0,0 +1,142 @@
+const { src, series, dest, watch } = require('gulp');
+const conf = require("./config.json");
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require("gulp-clean-css");
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+const terser = require("gulp-terser");
+const plumber = require("gulp-plumber");
+const browserSync = require("browser-sync").create();
+
+// BrowserSync
+function browsersyncServe(cb){
+    browserSync.init({
+      server: {
+        baseDir: "html/",
+        directory: true
+      }
+    });
+    cb();
+  }
+  
+  function browsersyncReload(cb){
+    browserSync.reload();
+    cb();
+  }
+  
+
+/*! =======================================================
+                    //CSS MINIFICATION STARTS
+========================================================= */
+
+//globalComponentcss minification
+
+function globalcompscss() {
+    return src(conf.src.srcCssTDRAbase + conf.src.sasssrc.globalComps)
+        .pipe(sass({
+          quietDeps: true
+          }).on("error", sass.logError))
+        .pipe(plumber())
+        .pipe(concat("globalcomps.css"))
+        .pipe(dest(conf.dest.destCssTDRAbase + conf.dest.cssdest.globalComps))
+        .pipe(
+            rename({
+                suffix: ".min",
+            })
+        )
+        .pipe(cleanCSS())
+        .pipe(postcss([cssnano()]))
+        .pipe(dest(conf.dest.destCssTDRAbase + conf.dest.cssdest.globalComps))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+function homepageCSS() {
+    return src(conf.src.srcCssTDRAbase + conf.src.sasssrc.homepage)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(plumber())
+        .pipe(concat("homepage.css"))
+        .pipe(dest(conf.dest.destCssTDRAbase + conf.dest.cssdest.homepage))
+        .pipe(
+            rename({
+                suffix: ".min",
+            })
+        )
+        .pipe(cleanCSS())
+        .pipe(postcss([cssnano()]))
+        .pipe(dest(conf.dest.destCssTDRAbase + conf.dest.cssdest.homepage))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+
+/*! =======================================================
+                    //JS MINIFICATION STARTS
+========================================================= */
+
+//global js minification
+function globalScripts() {
+    // Minify and copy all JavaScript (except vendor scripts)
+    return src(conf.globalScripts.src)
+        .pipe(plumber())
+        .pipe(concat("global.js"))
+        .pipe(dest(conf.dest.destJsTDRAbase + conf.dest.jsdest.globaljs))
+        .pipe(terser())
+        .pipe(
+            rename({
+                suffix: ".min",
+            })
+        )
+        .pipe(dest(conf.dest.destJsTDRAbase + conf.dest.jsdest.globaljs))
+        .pipe(browserSync.stream());
+}
+
+// //homepage js minification
+// function homepagescripts() {
+//     // Minify and copy all JavaScript (except vendor scripts)
+//     return gulp
+//         .src(conf.homepagescripts.src)
+//         .pipe(plumber())
+//         .pipe(concat("global.js"))
+//         .pipe(gulp.dest(conf.dest.destJsTDRAbase + conf.dest.jsdest.homepagejs))
+//         .pipe(terser())
+//         .pipe(
+//             rename({
+//                 suffix: ".min",
+//             })
+//         )
+//         .pipe(gulp.dest(conf.dest.destJsTDRAbase + conf.dest.jsdest.homepagejs))
+//         .pipe(browserSync.stream());
+// }
+
+
+function watchTask() {
+    // watching scripts/scss/twig/html files
+
+   // gulp.watch("../../../Feature/**/**/*.js", gulp.series(scripts, reload));
+   watch("**/*.html", browsersyncReload);
+   watch(['assets/scss/**/*.scss', 'assets/scripts/**/*.js'], series(browsersyncReload));
+}
+
+
+// Default Gulp task
+exports.TDRA = series(globalcompscss, globalScripts, homepageCSS, browsersyncServe, watchTask);
+
+// Gulp component Task
+
+// gulp.task(
+//     "TDRA-CSS",
+//     gulp.series(globalcompscss, homepage, browsersyncServe, watch),
+//     function (cb) {
+//         // Static Server
+//     }
+// );
+
+// gulp.task(
+//     "TDRA-JS",
+//     gulp.series(globalScripts, homepagescripts),
+//     function (cb) {
+//         // Static Server
+//     }
+// );
+
